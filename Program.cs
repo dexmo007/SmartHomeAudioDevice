b88c7c48b418e99a05bdf18c8aa1611252807143
Henrik Drefs <henrik.drefs@outlook.de>
2023-07-27 20:37:46 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using SmartHomeAudioDevice;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+
+    private readonly MMDeviceEnumerator deviceEnum = new();
+    private SmartHomeAudioDeviceNotificationClient? notificationClient;
+
+    /// <summary>
+    /// Registers a call back for Device Events
+    /// </summary>
+    /// <param name="client">Object implementing IMMNotificationClient type casted as IMMNotificationClient interface</param>
+    /// <returns></returns>
+    public int RegisterEndpointNotificationCallback([In][MarshalAs(UnmanagedType.Interface)] IMMNotificationClient client)
+    {
+        //DeviceEnum declared below
+        return deviceEnum.RegisterEndpointNotificationCallback(client);
+    }
+
+    /// <summary>
+    /// UnRegisters a call back for Device Events
+    /// </summary>
+    /// <param name="client">Object implementing IMMNotificationClient type casted as IMMNotificationClient interface </param>
+    /// <returns></returns>
+    public int UnRegisterEndpointNotificationCallback([In][MarshalAs(UnmanagedType.Interface)] IMMNotificationClient client)
+    {
+        //DeviceEnum declared below
+        return deviceEnum.UnregisterEndpointNotificationCallback(client);
+    }
+
+    private void Run(bool onlyShow)
+    {
+        foreach (var device in deviceEnum.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+        {
+            Console.WriteLine($"{device.DeviceFriendlyName} ({device.ID})");
+        }
+        if (onlyShow) { return; }
+        string ioBrokerApiUrl = "http://192.168.178.225:8087";
+        string stateId = "0_userdata.0.computer.audio_to_amplifier";
+        string[] audioDevices = { "{0.0.0.00000000}.{e9184462-38c7-41f9-96e5-41af37e7c02f}" };
+        notificationClient = new SmartHomeAudioDeviceNotificationClient(ioBrokerApiUrl, stateId, audioDevices);
+        deviceEnum.RegisterEndpointNotificationCallback(notificationClient);
+
+
+    }
+
+    private static void Main(string[] args)
+    {
+        new Program().Run(false);
+
+
+        Console.WriteLine("Listening...");
+        Console.ReadLine();
+    }
+}
---
+++ SmartHomeAudioDeviceNotificationClient.cs
@@ -0,0 +1,79 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeAudioDevice
+{
+    internal class SmartHomeAudioDeviceNotificationClient : NAudio.CoreAudioApi.Interfaces.IMMNotificationClient
+    {
+
+        private readonly HttpClient httpClient;
+        private readonly string ioBrokerApiUrl;
+        private readonly string stateId;
+        private readonly string[] audioDevices;
+        private bool? lastValue;
+
+        public SmartHomeAudioDeviceNotificationClient(string ioBrokerApiUrl, string stateId, string[] audioDevices)
+        {
+            if (System.Environment.OSVersion.Version.Major < 6)
+            {
+                throw new NotSupportedException("This functionality is only supported on Windows Vista or newer.");
+            }
+            this.httpClient = new HttpClient();
+            this.ioBrokerApiUrl = ioBrokerApiUrl;
+            this.stateId = stateId;
+            this.audioDevices = audioDevices;
+            this.lastValue = null;
+        }
+
+        private void SendUpdateToIoBroker(bool value)
+        {
+            if (lastValue != null && lastValue == value) {
+                Console.WriteLine("No need to update ioBroker");
+                return; 
+            }
+            httpClient.GetStringAsync($"{ioBrokerApiUrl}/set/{stateId}?value={(value ? "true" : "false")}&ack=true");
+            Console.WriteLine("Send update to ioBroker!");
+            lastValue = value;
+        }
+
+        public void OnDefaultDeviceChanged(DataFlow dataFlow, Role deviceRole, string defaultDeviceId)
+        {
+            if (dataFlow != DataFlow.Render)
+            {
+                return;
+            }
+            Console.WriteLine($"Default Audio Output Changed --> new default device is {defaultDeviceId}");
+            SendUpdateToIoBroker(audioDevices.Contains(defaultDeviceId));
+        }
+
+        public void OnDeviceAdded(string deviceId)
+        {
+            // Nothing to do
+        }
+
+        public void OnDeviceRemoved(string deviceId)
+        {
+
+            // Nothing to do
+        }
+
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            // Nothing to do
+        }
+
+
+
+        public void OnPropertyValueChanged(string deviceId, PropertyKey propertyKey)
+        {
+            // Nothing to do
+
+        }
+
+    }
+}
